9aeb15cc5812b580be4b6b4b8116ae09678197ef
weiyx <weiyx16@mails.tsinghua.edu.cn>
2019-07-30 18:21:00 -0400

    Finish character and word bbox extraction and OCR on word patches.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rev_Vis
+
+## Schedule
+
++ 29/07/2019
+> Finish axes extraction  
+> Try to use OCR under engine of [Tesseract](https://github.com/tesseract-ocr/tesseract) but not good  
+> According the axes to crop subchart and resize&rotate chart for OCR, still not excellent  
+
++ 30/07/2019  
+> Finish character extraction  
+> Merge characters belonging to a word to bbox together  
+> Run OCR on word image patch, and its augmentated counterparts (rotation and resize)  
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os  # glob
 import argparse
+import time
+import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import matplotlib.patches as mpatches
 from skimage import io as IMGIO
-from skimage.filters import sobel, threshold_otsu
+from skimage.filters import (sobel, threshold_otsu)
 from skimage.transform import (hough_line, hough_line_peaks,
                                probabilistic_hough_line, resize, rotate)
 from skimage.feature import canny
-from skimage.color import rgb2gray
-from skimage.measure import label as conn
-import numpy as np
+from skimage.color import (rgb2gray, label2rgb)
+from skimage.measure import (label, regionprops)
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
 import pyocr
 import pyocr.builders as ocrtools
 import utils.utils as utils
@@ -47,7 +54,7 @@ def axes_detection(img_gray, img_height, img_width):
                                      line_gap=10, theta=np.asarray([-PI/2, 0, PI/2]))
 
     # Save Result
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharex=True, sharey=True)
+    fig, axes = plt.subplots(1, 4, figsize=(20, 5), sharex=True, sharey=True)
     ax = axes.ravel()
 
     ax[0].imshow(img_gray, cmap=cm.gray)
@@ -67,17 +74,40 @@ def axes_detection(img_gray, img_height, img_width):
             line_exist.append(line)
             # 0 is x and 1 is y
             ax[2].plot((p0[0], p1[0]), (p0[1], p1[1]))
-    ax[2].set_xlim((0, img_gray.shape[1]))
-    ax[2].set_ylim((img_gray.shape[0], 0))
-    ax[2].set_title('Probabilistic Hough')
+    ax[2].set_xlim((0, edge_binary.shape[1]))
+    ax[2].set_ylim((edge_binary.shape[0], 0))
+    ax[2].set_title('Probabilistic Hough with replicated line remove')
+
+    ax[3].imshow(edge_binary * 0, cmap=cm.gray)
+    axes = []
+    for line_1 in line_exist:
+        for line_2 in line_exist:
+            if line_1 == line_2:
+                continue
+            # first need vertical
+            if (line_1[0][0] == line_1[1][0] and line_2[0][1] == line_2[1][1]) or \
+                    (line_1[0][1] == line_1[1][1] and line_2[0][0] == line_2[1][0]):
+                # second need to share a similar point
+                if points_near(line_1[0], line_2[0]) or points_near(line_1[0], line_2[1]) or \
+                        points_near(line_1[1], line_2[0]) or points_near(line_1[1], line_2[1]):
+                    axes.append(line_1)
+                    axes.append(line_2)
+                    ax[3].plot((line_1[0][0], line_1[1][0]), (line_1[0][1], line_1[1][1]))
+                    ax[3].plot((line_2[0][0], line_2[1][0]), (line_2[0][1], line_2[1][1]))
+                    break
+        if axes is not None:
+            break
+    ax[3].set_xlim((0, edge_binary.shape[1]))
+    ax[3].set_ylim((edge_binary.shape[0], 0))
+    ax[3].set_title('Axes Detection')
 
     for a in ax:
         a.set_axis_off()
-    plt.savefig('edge_gray.png')
+    # plt.savefig('edge_gray.png')
     plt.tight_layout()
     plt.show()
 
-    return line_exist
+    return axes
 
     '''
     ## Hough
@@ -147,14 +177,22 @@ def axes_detection(img_gray, img_height, img_width):
 
 def repli_lines(line_exist, p0, p1):
     for l in line_exist:
-        if l[0][0] == l[1][0] and p0[0] == p1[0]:
-            if abs(l[0][1] - p0[1]) < 10:
+        if (l[0][0] == l[1][0] and p0[0] == p1[0]) or (l[0][1] == l[1][1] and p0[1] == p1[1]):
+            if point_in_line_range(l, p0) and point_in_line_range(l, p1):
                 print('Remove Line! Exist Line: {}, Current Line: ({}, {})'.format(l, p0, p1))
                 return True
-        if l[0][1] == l[1][1] and p0[1] == p1[1]:
-            if abs(l[0][0] - p0[0]) < 10:
-                print('Remove Line! Exist Line: {}, Current Line: ({}, {})'.format(l, p0, p1))
-                return True
+    return False
+
+
+def point_in_line_range(l, p):
+    if points_near(l[0], p, 10) or points_near(l[1], p, 10):
+        return True
+    return False
+
+
+def points_near(p1, p2, dist=30):
+    if (abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])) < dist:
+        return True
     return False
 
 
@@ -216,13 +254,194 @@ def ocr(tool, img, cont='txt'):
 
 
 def ocr_image_preprocess(img, rotate_aug=True):
-    img_resized = resize(img, (img.shape[0] * 3, img.shape[1] * 3), anti_aliasing=True)
+    img_resized = resize(img, (img.shape[0] * 4, img.shape[1] * 4), anti_aliasing=True)
     if rotate_aug:
         img_pos = rotate(img_resized, PI/2 * Deg_2_Angle, resize=True)
         img_neg = rotate(img_resized, - PI/2 * Deg_2_Angle, resize=True)
         return [img_pos, img_neg, img_resized]
     else:
         return [None, None, img_resized]
+
+
+def chart_ocr(img, word_bbox, tool, pad=True):
+    txt = {}
+    for idx, bbox in enumerate(word_bbox):
+        minr, minc, maxr, maxc = bbox
+        if pad:
+            word_img = np.ones((2*(maxr - minr), 2*(maxc - minc)))
+            word_img[(maxr - minr)//2:(maxr - minr)//2 + (maxr - minr),
+                        (maxc - minc)//2:(maxc - minc)//2 + (maxc - minc)] = img[minr:maxr, minc:maxc]
+        else:
+            word_img = img[minr:maxr, minc:maxc]
+
+        # im = Image.fromarray((word_img * 255.0).astype('uint8'), mode='L')
+        # im.save('./word_bbox_%d.png' % idx)
+
+        img_aug = ocr_image_preprocess(word_img, rotate_aug=True)
+        txt[idx] = {'bbox': bbox}
+        for idx_img, word_img_aug in enumerate(img_aug):
+            if word_img_aug is not None:
+                txt[idx]['txt_%d' % idx_img] = ocr(tool, word_img_aug, 'txt')
+
+        # TODO: Need to vote in all three orientation
+    return txt
+
+
+def character_detection(img, name, loc_bias=[0, 0]):
+    # characters detection
+    img_height, img_width = img.shape
+    label_img, label_num = label(img, neighbors=None, background=None, return_num=True, connectivity=None)
+    image_label_overlay = label2rgb(label_img, image=img)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.imshow(image_label_overlay)
+
+    chara_bbox_cent = []
+    chara_bbox = []
+    chara_bbox_area = []
+    for region in regionprops(label_img):
+        # take regions with large enough areas
+        if (img_height*img_width) // 9 >= region.bbox_area >= 20 \
+                and (region.major_axis_length < 10.0 * region.minor_axis_length):
+            # draw rectangle around segmented coins
+            minr, minc, maxr, maxc = region.bbox
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                      fill=False, edgecolor='red', linewidth=1)
+
+            chara_bbox_cent.append(region.centroid)
+            chara_bbox.append(region.bbox)
+            chara_bbox_area.append(region.bbox_area)
+            ax.add_patch(rect)
+
+    plt.title(' Connection Components & Bboxes')
+    ax.set_axis_off()
+    # plt.savefig(name)
+    plt.tight_layout()
+    plt.show()
+
+    # convert local center to global center
+    from operator import add
+    return list(map(lambda x: list(map(add, x, loc_bias)), chara_bbox_cent)), \
+        list(map(lambda x: list(map(add, x, loc_bias + loc_bias)), chara_bbox)), \
+        chara_bbox_area
+
+
+def character_cluster(chara_cent, chara_bbox, chara_bbox_area, img):
+    chara_cent = np.asarray(chara_cent)
+    chara_bbox_area = np.asarray(chara_bbox_area)
+    chara_bbox = np.asarray(chara_bbox)
+    n_samples, n_features = chara_cent.shape
+    # min_score = 1e10
+    # kmeans_best = None
+    efficiency = []
+    inertia_last = 1e10
+    for n_digits in range(1, 15):
+        # print("n_digits: %d, \t n_samples %d, \t n_features %d" % (n_digits, n_samples, n_features))
+
+        kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
+        kmeans.fit(chara_cent, sample_weight=chara_bbox_area)
+
+        # inertia_ Sum of squared distances of samples to their closest cluster center.
+        # kmeans score: Opposite of the value of X on the K-means objective.
+        kmeans_score = - kmeans.score(chara_cent)
+        # print(' Score: {} Inertia: {}'.format(kmeans_score, kmeans.inertia_))
+        efficiency.append(kmeans.inertia_ / inertia_last)
+        inertia_last = kmeans.inertia_
+        # if kmeans_score < min_score:
+        #     min_score = kmeans_score
+        #     kmeans_best = kmeans
+
+    last_eff = list(filter(lambda x: x < 0.6, efficiency))[-1]
+    n_digits = efficiency.index(last_eff) + 1
+    print('Pick digit number as: {}' .format(n_digits))
+    kmeans_best = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
+    kmeans_best.fit(chara_cent, sample_weight=chara_bbox_area)
+
+    # merge bboxes which belong to one word together according to the bbox
+    labels = kmeans_best.labels_
+    word_bbox = {}
+    for idx, label in enumerate(labels):
+        bbox_add = chara_bbox[idx]
+        if label not in word_bbox.keys():
+            word_bbox[label] = bbox_add
+        else:
+            bbox_cur = word_bbox[label]
+            bbox_cur = bbox_merge(bbox_cur, bbox_add)
+            word_bbox[label] = bbox_cur
+
+    # merge near word bounding box just in case of the non-perfect of KMeans:
+    # and by the way, merge word in line together
+    word_bbox_postproc = []
+    for bbox_cur in word_bbox.values():
+        minr_c, minc_c, maxr_c, maxc_c = bbox_cur
+        if not word_bbox_postproc:
+            word_bbox_postproc.append(bbox_cur)
+            continue
+
+        bbox_add_list = []
+        for idx, bbox_exist in enumerate(word_bbox_postproc):
+            minr_e, minc_e, maxr_e, maxc_e = bbox_exist
+            if (line_overlap(minr_e, maxr_e, minr_c, maxr_c) and (abs(minc_c - maxc_e) < 10 or abs(maxc_c - minc_e) < 10)) \
+                or (line_overlap(minc_e, maxc_e, minc_c, maxc_c) and (abs(minr_c - maxr_e) < 10 or abs(maxr_c - minr_e) < 10)):
+                word_bbox_postproc.pop(idx)
+                bbox_exist = bbox_merge(bbox_exist, bbox_cur)
+                bbox_add_list.append(bbox_exist)
+
+        if not bbox_add_list:
+            bbox_add_list.append(bbox_cur)
+        word_bbox_postproc += bbox_add_list
+
+    print(' %d words bounding box detection' % len(word_bbox_postproc))
+    plt.figure(1, figsize=(10, 6))
+    plt.clf()
+    ax = plt.gca()
+    ax.xaxis.set_ticks_position('top')
+    ax.invert_yaxis()
+
+    plt.imshow(img, cmap=cm.gray)
+
+    plt.plot(chara_cent[:, 1], chara_cent[:, 0], 'b.', markersize=3)
+    # Plot the centroids as a white X
+    centroids = kmeans_best.cluster_centers_
+    plt.scatter(centroids[:, 1], centroids[:, 0],
+                marker='x', s=169, linewidths=3,
+                color='g', zorder=10)
+
+    for bbox_show in word_bbox_postproc:
+        minr, minc, maxr, maxc = bbox_show
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                  fill=False, edgecolor='red', linewidth=1)
+        ax.add_patch(rect)
+
+    plt.title('K-means clustering with words num of %d' % len(word_bbox_postproc))
+    # plt.savefig('word_detection.png')
+    plt.show()
+
+    return word_bbox_postproc
+
+
+def line_overlap(x1, y1, x2, y2, thre=None):
+    max_x = max(x1, x2)
+    min_x = min(x1, x2)
+    max_y = max(y1, y2)
+    min_y = min(y1, y2)
+    max_len = max_y - min_x
+    overlap = min_y - max_x
+    if thre is None:
+        min_len = min((y1-x1),(y2-x2))
+        if overlap > min_len / 2:
+            return True
+    else:
+        if overlap / max_len > thre:
+            return True
+    return False
+
+
+def bbox_merge(bbox_cur, bbox_add):
+    bbox_cur[0] = min(bbox_cur[0], bbox_add[0])
+    bbox_cur[1] = min(bbox_cur[1], bbox_add[1])
+    bbox_cur[2] = max(bbox_cur[2], bbox_add[2])
+    bbox_cur[3] = max(bbox_cur[3], bbox_add[3])
+    return bbox_cur
 
 
 def main(args):
@@ -234,6 +453,8 @@ def main(args):
         IMG = Image.open(args.input_chart_path)
         img = np.asarray(IMG, dtype='uint8')  # 8 bit RGB
         img_gray = rgb2gray(img)  # range in [0,1] float64
+        thresh = threshold_otsu(img_gray)
+        img_binary = img_gray < thresh  # [False or True] bool
         img_height, img_width = img_gray.shape
     except IOError:
         raise Exception(' Illegal chart file extension \n '
@@ -243,6 +464,7 @@ def main(args):
 
     # axes_detection
     axes_line = axes_detection(img_gray, img_height, img_width)
+    axes_line = axes_line[:2]
     print(' Axes location detected with location of {}'.format(axes_line))
 
     # OCR for printed text detection
@@ -255,54 +477,31 @@ def main(args):
         p0, p1 = line
         if p0[0] == p1[0]:
             # left of y-axis
-            tmp_mask = np.zeros_like(img_gray)
-            tmp_mask[:, :p0[0]] = 1
-            img_left_y = img_gray * tmp_mask
-            im = Image.fromarray((img_left_y * 255.0).astype('uint8'), mode='L')
+            img_left_y = np.zeros((img_binary.shape[0], p0[0]), dtype='uint8')
+            img_left_y = img_binary[:, :p0[0]]
 
-            label_img, label_num = conn(input, neighbors=None, background=None, return_num=True, connectivity=None)
-            # https://scikit-image.org/docs/dev/api/skimage.measure.html#skimage.measure.label
+            chara_cent, chara_bbox, chara_bbox_area = character_detection(img_left_y, 'conn_comp_left_y.png', [0, 0])
 
-            im.save('./img_left_y.png')
-            img_aug = ocr_image_preprocess(img_left_y, rotate_aug=True)
-            txt_left_y = []
-            for i in img_aug:
-                if i is not None:
-                    txt_left_y_cur = ocr(tool, i, 'txt')
-                    txt_left_y.append(txt_left_y_cur)
-                    print(" TXT for left from OCR {}" .format(txt_left_y_cur))
+            word_bbox = character_cluster(chara_cent, chara_bbox, chara_bbox_area, img_binary)
+
+            word_in_chart = chart_ocr(img_gray, word_bbox, tool)
+            print(" TXT for left from OCR {}".format(word_in_chart))
+
         if p0[1] == p1[1]:
             # down of x-axis
-            tmp_mask = np.zeros_like(img_gray)
-            tmp_mask[p0[1]:-1, :] = 1
-            img_down_x = img_gray * tmp_mask
-            im = Image.fromarray((img_down_x * 255.0).astype('uint8'), mode='L')
-            im.save('./img_down_x.png')
-            img_aug = ocr_image_preprocess(img_down_x, rotate_aug=True)
-            txt_down_x = []
-            for i in img_aug:
-                if i is not None:
-                    txt_down_x_cur = ocr(tool, i, 'txt')
-                    txt_down_x.append(txt_down_x_cur)
-                    print(" TXT for below from OCR {}" .format(txt_down_x_cur))
+            img_down_x = np.zeros((img_binary.shape[0] - p0[1], img_binary.shape[1]), dtype='uint8')
+            img_down_x = img_binary[p0[1]:-1, :]
 
+            chara_cent, chara_bbox, chara_bbox_area = character_detection(img_down_x, 'conn_comp_down_x.png', [p0[1], 0])
 
-    # output:
-    # TXT for below from OCR adAL
-    # UBAILIW
-    # uobep
-    # Jep
-    # suodg
-    #
-    # TXT for below from OCR Sports Car
-    #
-    # Type
-    # TXT for below from OCR Sports Car
-    #
-    # Type
-    # TXT for left from OCR uojes) Jad sai AiD
-    # TXT for left from OCR City Miles Per Gallon
-    # TXT for left from OCR uojes) Jad sai AiD
+            word_bbox =character_cluster(chara_cent, chara_bbox, chara_bbox_area, img_binary)
+
+            word_in_chart = chart_ocr(img_gray, word_bbox, tool)
+            print(" TXT for down from OCR {}".format(word_in_chart))
+
+            # im = Image.fromarray((img_down_x * 255.0).astype('uint8'), mode='L')
+            # im.save('./img_down_x.png')
+
 
 if __name__ == '__main__':
     args_input = parser.parse_args()
